e66ecc8d6aac66e99fcd135431a3dd94a2447566
Joseph Fusco <josephfusco@users.noreply.github.com>
2016-01-08 07:47:18 -0500

    Update wp-git-status.php

--- wp-git-status.php
+++ wp-git-status.php
@@ -7,6 +7,7 @@ Version:        1.0.0
 Author:         Joseph Fusco
 Author URI:     http://josephfus.co/
 License:        GPLv2 or later
+License URI:    http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 if ( !defined( 'ABSPATH' ) )
