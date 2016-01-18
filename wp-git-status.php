a7d208fd98b2fcfe46fc40df8c7ce9468ea2cd8f
Joseph Fusco <hello@josephfus.co>
2016-01-18 15:50:31 -0500

    Enqueue stylesheet.

--- wp-git-status.php
+++ wp-git-status.php
@@ -1,29 +1,43 @@
 <?php
-/*
-Plugin Name:    WP Git Status
-Plugin URI:     http://github.com/josephfusco/wp-git-status/
-Description:    Show active theme git branch and commit hash in the toolbar.
-Version:        1.0.0
-Author:         Joseph Fusco
-Author URI:     http://josephfus.co/
-License:        GPLv2 or later
-License URI:    http://www.gnu.org/licenses/gpl-2.0.html
-*/
+/**
+ * Plugin Name:    WP Git Status
+ * Plugin URI:     http://github.com/josephfusco/wp-git-status/
+ * Description:    Show active theme git branch and commit hash in the toolbar.
+ * Version:        1.0.0
+ * Author:         Joseph Fusco
+ * Author URI:     http://josephfus.co/
+ * License:        GPLv2 or later
+ * License URI:    http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-if ( !defined( 'ABSPATH' ) )
-	exit;
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 add_action( 'admin_bar_menu', 'wpgs_git_info', 900 );
+add_action( 'wp_enqueue_scripts', 'wpgs_enqueue' );
+add_action( 'admin_enqueue_scripts', 'wpgs_enqueue' );
 
 /**
+ *
+ * Enqueue stylesheet
+ *
+ */
+function wpgs_enqueue() {
+	wp_enqueue_style( 'wpgs-style', plugins_url( '/css/style.css' , __FILE__ ) );
+}
+
+/**
+ *
  * Execute git commands
+ *
  */
 function wpgs_git_rev( $path ) {
 	return shell_exec( 'cd ' . $path . ' && git rev-parse --short HEAD && git rev-parse --abbrev-ref HEAD 2>&1' );
 }
 
 /**
+ *
  * Get git info
+ *
  */
 function wpgs_get_git_info() {
 	$theme_directory = exec( 'cd ' . get_stylesheet_directory() . ' && pwd 2>&1' );
@@ -32,7 +46,7 @@ function wpgs_get_git_info() {
 
 	// Check if commits are present
 	if(file_exists($git_index)){
-		return wp_get_theme() . ': <strong style="font-family:monospace">' . wpgs_git_rev( $theme_directory ) . '</strong>';
+		return wp_get_theme() . ': <strong>' . wpgs_git_rev( $theme_directory ) . '</strong>';
 	} elseif(file_exists($git)) {
 		return wp_get_theme() . ': no commit history';
 	} else {
@@ -41,14 +55,18 @@ function wpgs_get_git_info() {
 }
 
 /**
+ *
  * Add git info to toolbar
+ *
  */
-function wpgs_git_info($wp_admin_bar) {
+function wpgs_git_info( $wp_admin_bar ) {
 	$git_info = wpgs_get_git_info();
 	$args = array(
-		'id'    => 'git_status',
-		'title' => $git_info,
-		'meta'  => array( 'class' => 'first-toolbar-group' ),
+		'id'    => 'git-status',
+		'title' => '<span class="ab-icon"></span><span class="ab-label">' . $git_info . '</span>',
+		'meta'  => array(
+            'class' => 'git-status'
+        )
 	);
 	$wp_admin_bar->add_node( $args );
 }
